38d35ac668dc6b71e78b10edd0d64f924856ea06
wim forton <wim.forton@gmail.com>
2024-02-16 17:04:57 +0100

    threejs installed

---
+++ src/app/threejs/testbench/test/test.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import * as THREE from 'three';
+
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+
+}
